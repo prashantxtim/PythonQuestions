71fd272851d741d07cc94639d3c87b06d0ffa723
prashanttimilsena <prashanttimilsena59@gmail.com>
2021-02-25 09:22:22 +0545

    Initial commit

---
+++ week2.1/week2.1_file1.py
@@ -0,0 +1,7 @@
+#Write a program that takes 3 numbers and print their sum. Every number is given on a separate line.
+# This program reads three numbers and prints their sum:
+
+a = int(input("Enter a value of a "))
+b = int(input("Enter a value of b "))
+c = int(input("Enter a value of c "))
+print(a + b + c)
