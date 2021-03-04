024239274283389c5a5840bff99c3ba8ae9881b7
prashanttimilsena <prashanttimilsena59@gmail.com>
2021-03-04 10:11:19 +0545

    question4 add

--- week2.1/question4.py
+++ week2.1/question4.py
@@ -3,3 +3,8 @@
 #number of minutes (between 0 and 59).
 #for example, if N = 150 ,then 150 minutes have passed since midnight -i:e now is 2:30 am.So the program should print 2 30.
 
+hours = int(input("How many hours have passed after midnight? "))
+minutes = int(input("How many minutes have passed after midnight? "))
+hours_in_minutes = hours*60
+minutes_after_midnight = (hours_in_minutes)+(minutes)
+print(f"{minutes_after_midnight} minutes have passed after midnight")
